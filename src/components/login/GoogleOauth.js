aa1ca9448778e6cb8587cb48a715b0ae9acfd891
Hamza Ahmad <ahmad.hamza@systemsltd.com>
2023-03-03 14:34:01 +0500

    update hooks

--- src/components/login/GoogleOauth.js
+++ src/components/login/GoogleOauth.js
@@ -2,6 +2,10 @@ import React, { useEffect } from "react";
 import jwt_decode from "jwt-decode";
 import { useNavigate } from "react-router-dom";
 const GoogleOauth = () => {
+  const { handleGoogle, loading, error } = useFetch(
+    "http://localhost:5152/login"
+  );
+
   const navigate = useNavigate();
   useEffect(() => {
     const script = document.createElement("script");
